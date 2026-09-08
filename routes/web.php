19a6167954b6f08967e74062bf15a410203d89a2
Gabi <engs-gabrielliferreira@camporeal.edu.br>
2026-09-07 21:08:29 -0300

    Atividade_01 - Rotas com Texto

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sobre', function () {
+    return 'Pagina de informações';
+});
+
+Route::get('/alunos', function () {
+    return 'Pagina de Alunos';
+});
+
+Route::get('/contato', function () {
+    return 'Pagina de Contato';
+});
