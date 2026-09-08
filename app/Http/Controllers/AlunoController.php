ff5995cc4321c1b9c6174b401c19ba15b2020e47
Gabi <engs-gabrielliferreira@camporeal.edu.br>
2026-09-07 22:26:44 -0300

    Atividade_06 - No AlunoController os métodos retornam as suas views

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -7,23 +7,23 @@ use Illuminate\Http\Request;
 class AlunoController extends Controller
 {
     public function index() {
-
+        return view('alunos.index');
     }
 
     public function show(){
-
+        return view('alunos.show');
     }
 
     public function create() {
-
+        return view('alunos.create');
     }
 
     public function store() {
-
+        
     }
 
     public function edit() {
-
+        return view('alunos.edit');
     }
 
     public function update() {
