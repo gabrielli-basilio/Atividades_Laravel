d5d5e7064988ae88265293503ce58f82481fd355
Gabi <engs-gabrielliferreira@camporeal.edu.br>
2026-09-09 22:25:48 -0300

    Atividade_17 - Relacionamento dos models com chave estrangeira

--- app/Models/Curso.php
+++ app/Models/Curso.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Curso extends Model
 {
     protected $fillable = ['nome'];
+
+    public function alunos()
+    {
+        return $this->hasMany(Aluno::class);
+    }
 }
