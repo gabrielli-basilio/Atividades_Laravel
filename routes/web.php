e838cd258f72a1d9ee38e58671de63ea286407fb
Gabi <engs-gabrielliferreira@camporeal.edu.br>
2026-09-09 23:17:34 -0300

    Atividade-18 - Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,85 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AlunoController;
-use App\Http\Controllers\CursoController;
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
 
-/* Route::get('/sobre', function () {
-    return 'Pagina de informações';
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/alunos', function () {
-    return 'Pagina de Alunos';
-});
-
-Route::get('/contato', function () {
-    return 'Pagina de Contato';
-}); */
-
-Route::get('/sobre', function () {
-    return view('sobre');
-});
-
-Route::get('/contato', function () {
-    return view('contato');
-});
-
-Route::get('/produto/{id}', function ($id) {
-    return "Número do produto: $id";
-});
-
-Route::get('/categoria/{id}', function ($id) {
-    return "Número da categoria: $id";
-});
-
-Route::get('/usuario/{id}', function ($id) {
-    return "Número do usuário: $id";
-});
-
-// Rotas CRUD para Alunos
-Route::get('/alunos', [
-    AlunoController::class, 'index'
-]);
-
-Route::get('/alunos/create', [
-    AlunoController::class, 'create'
-]);
-
-// Rota para consultas específicas de alunos
-Route::get('/alunos/consultas', [
-    AlunoController::class, 'consultas'
-]);
-
-Route::get('/alunos/{id}', [
-    AlunoController::class, 'show'
-]);
-
-Route::get('/alunos/{id}/edit', [
-    AlunoController::class, 'edit'
-]);
-
-Route::post('/alunos', [
-    AlunoController::class, 'store'
-]);
-
-Route::put('/alunos/{id}', [
-    AlunoController::class, 'update'
-]);
-
-Route::delete('/alunos/{id}', [
-    AlunoController::class, 'destroy'
-]);
-
-// Rota para exibir os alunos matriculados em um curso específico
-Route::get('/cursos/{id}/alunos', 
-    [CursoController::class, 'alunosPorCurso'
-]);
-
-// Rota que exibe os cursos
-Route::get('/cursos', [
-    CursoController::class, 'listaCursos'
-]);
+require __DIR__.'/auth.php';
